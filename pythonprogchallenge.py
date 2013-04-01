d435f7c6bace4f12f0932e351b159533d88d18d5
Sam Gan <gan.jingying@dhs.sg>
2013-04-01 22:58:00 +0800

    Converted XML to JSON but cannot make HTML
    Unable to complete this due to lack of knowledge and poor stomach...
    Very poor stomach... T-T

--- pythonprogchallenge.py
+++ pythonprogchallenge.py
@@ -1,16 +1,21 @@
 # Filename: pythonprogchallenge
 # Author: Gan Jing Ying
 # Created: 20130331
-# Modified: 20130331
+# Modified: 20130401
 # Description: Program that extracts all the components in an XML file
 
 # import stuff
 from xml.dom import minidom
+import json
 import re
+import urllib.request
+
+# define variables
+info = []
 
 # get your files
-infile = open("file.xml", "r")
-xml = infile.read()
+response = urllib.request.urlopen('http://www.dhs.sg/rss/what%2527s-new%3F-19.xml')
+xml = response.read()
 
 # gets all XML as a string
 # the "parseString" function will treat the stuff in 'xml' as a string
@@ -21,15 +26,27 @@ parts = xml_data[0].getElementsByTagName('item')
 
 # loop all items
 for part in parts:
-    # get title
-    title = part.getElementsByTagName('title')[0].firstChild.nodeValue.strip()
-    # get link
-    link = part.getElementsByTagName('link')[0].firstChild.nodeValue.strip()
+    # define dictionary
+    group = {}
     # get description
     description = part.getElementsByTagName('description')[0].firstChild.wholeText.strip()
     description = re.sub("<[^>]*>", "", description)
     description = description[:-10]
-    # display info
-    print "\n".join([title, link, description, ""])
+    group["description"] = description
+    # get link
+    link = part.getElementsByTagName('link')[0].firstChild.nodeValue.strip()
+    group["link"] = link
+    # get title
+    title = part.getElementsByTagName('title')[0].firstChild.nodeValue.strip()
+    group["title"] = title
+    # add group into info
+    info.append(group)
 
-infile.close()
+encoded = json.dumps(info, indent=2)
+print(encoded)
+
+# write info into JSON document
+outfile = open("jsonfile.json", encoding='utf-8', mode='w')
+outfile.write(encoded)
+
+outfile.close()
